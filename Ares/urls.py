d95926ebc25e824f72f4835899ca1bcced1bb7fc
wangzengping <873664730@qq.com>
2020-12-05 09:27:52 +0800

    cmdb

--- Ares/urls.py
+++ Ares/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cmdb import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('index/', views.index())
 ]
